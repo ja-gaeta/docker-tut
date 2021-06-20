bfacaa9111fdef4d4ad513c90fa6acfbabb0c120
gaeta <ja.gaeta1@gmail.com>
2021-06-20 00:36:45 -0300

    added express, dockerignore and docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:15
 WORKDIR /app
 COPY package.json .
 RUN npm install
-COPY . ./
 ENV PORT 3000
 EXPOSE $PORT
+COPY . .
 CMD ["npm", "run", "start"]
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const http = require('http')
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-  res.end('<h1>home page!!!</h1>')
+const app = express()
+
+app.get("/", (req,res)=>{
+  res.send("<h2>Hi There</h2>")
 })
 
 const port = process.env.PORT || 3000
 
-server.listen(port, ()=>console.log(`listening on port ${port}`))
+app.listen(port, ()=>console.log(`listening on port ${port}`))
